34a5fd88571beacf8d0a6a719ba92f5676477d34
Det Hsieh <danix914@gmail.com>
2020-10-15 14:30:36 +0800

    change directory to script folder

--- script.sh
+++ script.sh
@@ -3,6 +3,7 @@
 RATEFILE="exchange_rate.txt"
 
 main() {
+    cd "$(dirname "$0")"
     local rate=$(PYTHONIOENCODING=utf-8 .rate/bin/python exchange.py)
     echo "$(date "+%Y%m%d-%H%M%S") ${rate}" > "${RATEFILE}"
 }
